e8e31997f3d464214561ad850380dff3794df58e
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-01-21 02:01:43 +0300

    fix: add folderExists in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,8 +3,21 @@ const path = require('path');
 const fsPromises = fs.promises;
 const pathToNewFolder = path.join(__dirname, 'files-copy');
 
+async function folderExists(folderPath) {
+  try {
+    await fsPromises.access(folderPath);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 async function copyDir() {
   try {
+    const exists = await folderExists(pathToNewFolder);
+    if (exists) {
+      await fsPromises.rm(pathToNewFolder, { recursive: true, force: true });
+    }
     await fsPromises.mkdir(pathToNewFolder, { recursive: true });
     const files = await fsPromises.readdir(path.join(__dirname, 'files'));
     for (const file of files) {
